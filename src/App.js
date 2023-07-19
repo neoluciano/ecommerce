35da5999e1dfcfa7f809fd3dbcb975bd4a99edb6
neoluciano <luciano.pereyra@gmail.com>
2023-07-18 21:57:14 -0300

    ItemListContainer sin estilos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar/NavBar.js";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.js";
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <ItemListContainer greeting={'Bienvenidos'}/>
     </div>
     
   );
