11285e9d2a3d73bab510d75b7cc53f6d65b51fbd
neoluciano <luciano.pereyra@gmail.com>
2023-07-18 21:29:14 -0300

    NavBar sin estilos.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar (){
+    return (
+
+        <nav>
+            <h3>Neo Ecommerce</h3>
+            <div>
+                <button>Motorola</button>
+                <button>Apple</button>
+                <button>Samsung</button>
+            </div>
+        </nav>
+
+    )
+}
+
+export default NavBar;
