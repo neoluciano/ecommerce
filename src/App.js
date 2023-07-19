8f2e292140aa545eb17ba972f7ce2b60206b58e1
neoluciano <luciano.pereyra@gmail.com>
2023-07-19 00:18:38 -0300

    Primeros estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar/NavBar.js";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer.js";
+import "bulma/css/bulma.css"; //Cuando importo una hoja de estilos, no hace falta guardarla en una variable.
 
 function App() {
   return (
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,26 @@
 import CartWidget from "../CartWidget/CartWidget";
+import logoEcommerce from "./assets/logoEcommerce.png";
 
 function NavBar (){
     return (
 
-        <nav>
-            <h3>Neo Ecommerce</h3>
-            <div>
-                <button>Motorola</button>
-                <button>Apple</button>
-                <button>Samsung</button>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+            <a className="navbar-item" href="http://www.google.com">
+                <img src={logoEcommerce} alt="Logo Neo Ecommerce"/>
+                <h3>Neo Ecommerce</h3>
+            </a>
+            
+            <div className="navbar-menu">
+                <a className="navbar-item">Motorola</a>
+                <a className="navbar-item">Apple</a>
+                <a className="navbar-item">Samsung</a>
+                
             </div>
             <CartWidget/>
+            </div>
+            
+            
         </nav>
 
     )
